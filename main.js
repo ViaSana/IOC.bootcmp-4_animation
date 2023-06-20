c3b738c876f8cdf0abeee954fd714261445c3e11
playfluent <joe@playfluent.com>
2023-06-20 17:54:25 +0800

    subpoint animation

--- main.js
+++ main.js
@@ -30,13 +30,38 @@ function toggleSubPoints() {
     gsap.to(subPoints, {
         width: "auto",
         opacity: 1,
-        padding: '2px 1.5rem'
+        padding: '0'
     })
   }
 }
-
 // finally link it to the click action of the burger button
 object.addEventListener('mouseenter', toggleSubPoints)
 object.addEventListener('mouseleave', toggleSubPoints)
+})
 
+//  first we are finding all of the elements that have a class of scroll-in-from-bottom
+const headings = document.querySelectorAll(".gallery-scroll")
+
+// for each of them we are going to attach a gsap animation ( like a jekyll for loop just in js :D )
+headings.forEach(object => {
+
+// we start with a gsap timeline
+    let tl = gsap.timeline({
+    // here's where we start attching the animation to the scroll
+      scrollTrigger: {
+      	// this is what is triggering the start of the animation
+          trigger: object,
+        	// this one takes in to bits of info. the first is the part of the trigger element thats causing the trigger to start and the second one is where on the screen. both of them can be either top, center, bottom or a percentage (% are from the top of the container( as in trigger element or screen) )   
+          start: "top bottom",
+			// same as start but the end point of the animation          
+          end: "top center",
+          // now this is the most important line of code its what turns the triggers from a start point in to locking it to the scroll
+          scrub: true,
+          // last but not least this helps us to visualize the script
+          markers: true,
+      }
+    }).from(object,{
+        bottom:-300,
+        opacity:0
+    })
 })
